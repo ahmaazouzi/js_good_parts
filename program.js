d8117b51eafef3134e604dae4c80c916ba26dff8
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2019-03-21 00:44:49 -0400

    gp

--- program.js
+++ program.js
@@ -1,2 +1,11 @@
-document.writeln("Javascript .. beautiful .. language .. \
-	buried under a steaming pile of good intentions \u{1F98B}.");
+document.writeln("Javascript: a beautiful language \
+	buried under a steaming pile of good intentions and blunders \u{1F98B}.");
+
+var a = {
+	lala : [1,2],
+	baba : "ahmed",
+	pricesz: 22
+};
+for (i in a) 
+	if (a.hasOwnProperty(i))
+		console.log(i + "\t: " + a[i]);
